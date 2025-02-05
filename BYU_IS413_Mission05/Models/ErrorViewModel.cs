8269e4716a1a68ef080336e779aedffadd24b8d9
Jack Estes <jackestes@Jacks-MBP.app.byu.edu>
2025-02-05 08:47:07 -0700

    Initial commit

---
+++ BYU_IS413_Mission05/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BYU_IS413_Mission05.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
